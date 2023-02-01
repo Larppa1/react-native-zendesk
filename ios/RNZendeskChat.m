9f0581b7ef8f91dfd51737268f66f7fe623b957c
Larppa1 <lari.juva7@gmail.com>
2023-02-01 10:43:34 +0200

    ios/RNZendeskChat.m: Changes to init

--- ios/RNZendeskChat.m
+++ ios/RNZendeskChat.m
@@ -140,9 +140,9 @@ RCT_EXPORT_METHOD(setNotificationToken:(NSData *)deviceToken) {
 
     ZDKCustomField *customFieldOne = [[ZDKCustomField alloc] initWithFieldId:@1234567 value:@"some_value"];
     ZDKRequestUiConfiguration * config = [ZDKRequestUiConfiguration new];
-    config.subject = "iOS Ticket"
-    config.tags = ["ios", "mobile"]
-    config.customFields = [customFieldOne]
+    config.subject = "iOS Ticket";
+    config.tags = ["ios", "mobile"];
+    /* config.customFields = [customFieldOne] */
 
     /* UIViewController *requestController = [ZDKRequestUi buildRequestUiWith:@[config]];
     [self.navigationController pushViewController:requestController animated:YES]; */
